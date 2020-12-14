66cc1c8202925e202d64af61a8355f81ea8be052
Apoorv Kansal <apoorv.k.kansal@gmail.com>
2020-12-14 12:44:05 +1100

    Setup frontend infrastructure

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Widget from "./components/Widget";
+
+ReactDOM.render(
+  <Widget />,
+  document.getElementById("output")
+);
