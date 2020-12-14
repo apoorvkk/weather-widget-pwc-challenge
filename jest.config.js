f2ec49ea86d4e996fbdbf149de5c016b7b480ffd
Apoorv Kansal <apoorv.k.kansal@gmail.com>
2020-12-14 14:31:14 +1100

    Setup testing infrastructure

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect"
+  ],
+
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+
+  // Module file extensions for importing
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  restoreMocks: true,
+  clearMocks: true,
+  resetMocks: true
+};
